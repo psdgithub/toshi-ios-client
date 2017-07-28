1fd16fd264fac166d1316ed3d71859aae313064c
marijnschilling <marijnschilling@gmail.com>
2017-07-28 13:59:41 +0200

    remove self!

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -4,14 +4,14 @@ import UIKit
 class Tests: XCTestCase {
 
     func testExample() {
-        let expect = self.expectation(description: "get ethereum rate")
+        let expect = expectation(description: "get ethereum rate")
         EthereumAPIClient.shared.getRate { decimal in
             XCTAssertNotNil(decimal)
             expect.fulfill()
 
          }
 
-        self.waitForExpectations(timeout: 100)
+        waitForExpectations(timeout: 100)
 
     }
 }

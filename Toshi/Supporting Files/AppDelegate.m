3cea38d044bab09793c13f56d305aa5f85b77d44
marijnschilling <marijnschilling@gmail.com>
2017-08-10 11:36:13 +0200

    Make the testtarget check more verbose

--- Toshi/Supporting Files/AppDelegate.m
+++ Toshi/Supporting Files/AppDelegate.m
@@ -46,6 +46,14 @@ NSString *const RequiresSignIn = @"RequiresSignIn";
 @synthesize voipToken = _voipToken;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    BOOL isAppRunFromTests = (NSClassFromString(@"XCTestCase") != nil);
+    if (isAppRunFromTests) {
+        self.window = nil;
+
+        return NO;
+    }
+
     NSString *tokenChatServiceBaseURL = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"TokenChatServiceBaseURL"];
     [OWSSignalService setBaseURLPath:tokenChatServiceBaseURL];
 
